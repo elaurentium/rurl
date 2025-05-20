e3086408add702e7f07fd121072e2635a8bf7035
evandro.limeira <evandro.limeira@itadvanced.com.br>
2025-05-20 12:04:17 -0300

    feat: montando script

---
+++ script/main.py
@@ -0,0 +1,14 @@
+import sys
+import requests
+
+def main():
+    if len(sys.argv) < 2:
+        print("Use: "  + sys.argv[0] + " [Options] URL")
+        print("  -X, --request <method>    Specify the HTTP request method (GET, POST, etc.)")
+        print("  -H, --header <header>     Add a custom header to the request")
+        print("  -d, --data <data>        Send data with the request")
+        print("  -v, --verbose             Show verbose output")
+        print("  -o, --output <file>    Save the response to a file")
+        print("  -t, --timeout <seconds>  Define a timeout for the request")
+
+        sys.exit(1)
